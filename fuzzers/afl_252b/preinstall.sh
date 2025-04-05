c20379a2a7a47360d13b18862c2a5ff466d44c5b
dev <dev@slice.local>
2025-04-05 12:43:37 +0200

    clang

--- fuzzers/afl_252b/preinstall.sh
+++ fuzzers/afl_252b/preinstall.sh
@@ -2,7 +2,7 @@
 set -e
 
 apt-get update && \
-    apt-get install -y make build-essential clang-9 git wget
+    apt-get install -y make build-essential clang-8 git wget
 
 update-alternatives \
   --install /usr/lib/llvm              llvm             /usr/lib/llvm-9  20 \
@@ -29,6 +29,6 @@ update-alternatives \
     --slave   /usr/bin/llvm-tblgen       llvm-tblgen      /usr/bin/llvm-tblgen-9
 
 update-alternatives \
-  --install /usr/bin/clang                 clang                  /usr/bin/clang-9     20 \
-  --slave   /usr/bin/clang++               clang++                /usr/bin/clang++-9 \
-  --slave   /usr/bin/clang-cpp             clang-cpp              /usr/bin/clang-cpp-9
+  --install /usr/bin/clang                 clang                  /usr/bin/clang-8     20 \
+  --slave   /usr/bin/clang++               clang++                /usr/bin/clang++-8 \
+  --slave   /usr/bin/clang-cpp             clang-cpp              /usr/bin/clang-cpp-8
